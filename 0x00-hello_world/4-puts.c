952a99c6608bec17757edd65ad290950379a27b8
LeoMcBills <leokinyera9@gmail.com>
2022-12-08 06:24:30 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  * Using puts to display a string
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0) ;
+	return (0);
 }
