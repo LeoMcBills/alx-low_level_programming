e201b4a2c11cc5438a2a6845ceead7efd75bd5ba
LeoMcBills <leokinyera9@gmail.com>
2022-12-09 07:13:21 -0800

    A program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		if((x != 'q' && x != 'e') && x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
