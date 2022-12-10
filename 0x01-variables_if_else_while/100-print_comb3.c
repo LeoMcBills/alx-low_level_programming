bac490c622553b3fe5090452e9717495b16b42c9
LeoMcBills <leokinyera9@gmail.com>
2022-12-09 17:43:08 -0800

    A program that prints all possible different combinations of two digits in ascending order

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print combinatiins of digits in ascending order without repetions
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
