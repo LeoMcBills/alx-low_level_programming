194a671f8aa5a8a1325322c4fdb7f8a394bcaa6e
LeoMcBills <leokinyera9@gmail.com>
2022-12-13 18:09:25 -0800

    prints the first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints first 50 fibonacci numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int total, x, y, z;
+
+	x = 0;
+	y = 1;
+	z = 0;
+	total = 0;
+	while (z < 50)
+	{
+		total = x + y;
+		printf("%ld", total);
+		if (z != 0)
+		{
+			printf(", ");
+		}
+		x = y;
+		y = total;
+		z++;
+	}
+	printf("\n");
+	return (0);
+}
