e5b259a37ff63133fbd20cd8c704db94e6283c35
LeoMcBills <leokinyera9@gmail.com>
2022-12-15 07:07:20 -0800

    Prints integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,7 +4,16 @@
  * @n: The argument passed to the function is of integer type
  */
 
-void print_number(signed int n)
+void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((n / 10) > 0)
+		print_num(x / 10);
+	_putchar((x % 10) + '0');
 }
