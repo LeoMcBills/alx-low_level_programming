1d4e2a41c50c33f7e2bdce52b31391083e2a9f0d
LeoMcBills <leokinyera9@gmail.com>
2022-12-15 07:17:51 -0800

    Prints integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,8 @@
 
 void print_number(int n)
 {
-	unsigned int x = n;
+	signed int x = n;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		x = -x;
-	}
 	if ((n / 10) > 0)
 		print_num(x / 10);
 	_putchar((x % 10) + '0');
