35a1f738ae8bde8e1a089598074ca4af43974b8e
LeoMcBills <leokinyera9@gmail.com>
2022-12-19 03:45:15 -0800

    This function takes a pointer to an int as parameter and updates the value it prints to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - updates a variable to 98
  * @*n: parameter of int datatype
- * @: parameter for pointer variable of integer datatype
+ * @n: parameter for pointer variable of integer datatype
  * Return: 0
  */
 
