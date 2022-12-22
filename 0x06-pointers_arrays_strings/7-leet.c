51df5f7cf104e3e81d0ce14c83e9290d87fcfb33
LeoMcBills <leokinyera9@gmail.com>
2022-12-22 03:44:42 -0800

    A  function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encodes a string
+ * @str: Parameter of string to be encoded
+ * Return: Encoded string
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] || str[i] - 32 == leet[j])
+			{
+				str[i] = j + '0';
+			}
+		}
+	}
+	return (str);
+}
