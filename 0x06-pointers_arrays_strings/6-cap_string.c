40d4eb642bae50c49a814320da4b8b42ce689714
LeoMcBills <leokinyera9@gmail.com>
2022-12-23 13:00:26 -0800

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,24 +10,24 @@ char *cap_string(char *str)
 	int count;
 
 	count = 0;
-	while (s[count] != '\0')
+	while (str[count] != '\0')
 	{
-		if (s[0] >= 97 && s[0] <= 122)
+		if (str[0] >= 97 && str[0] <= 122)
 		{
-			s[0] = s[0] - 32;
+			str[0] = str[0] - 32;
 		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-				|| s[count] == '{' || s[count] == '}')
+		if (str[count] == ' ' || str[count] == '\t' || str[count] == '\n'
+				|| str[count] == ',' || str[count] == ';' || str[count] == '.'
+				|| str[count] == '.' || str[count] == '!' || str[count] == '?'
+				|| str[count] == '"' || str[count] == '(' || str[count] == ')'
+				|| str[count] == '{' || str[count] == '}')
 		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			if (str[count + 1] >= 97 && str[count + 1] <= 122)
 			{
-				s[count + 1] = s[count + 1] - 32;
+				str[count + 1] = str[count + 1] - 32;
 			}
 		}
 		count++;
 	}
-	return (s);
+	return (str);
 }
