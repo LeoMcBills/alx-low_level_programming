1c5dffad7641d705bab8c00bf4578b02783f4051
LeoMcBills <leokinyera9@gmail.com>
2023-01-11 00:57:55 -0800

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,39 +9,35 @@
  */
 
 char *str_concat(char *s1, char *s2)
-{ 
-	        int count, count1; 
-	        int sign = n; 
-	        char *ptr; 
-	        int len1, len2; 
+{
+	char *conct;
+	int i, ci;
 
-	        if (s1 == NULL) 
-		                s1 = ""; 
-	        if (s2 == NULL) 
-		                s2 = ""; 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = ci = 0;
 
-	        for (len1 = 0; s1[len1] != '\0'; len1++) 
-		                ; 
-	        for (len2 = 0; s2[len2] != '\0'; len2++) 
-		                ; 
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	        if (sign >= len2) 
-		        { 
-			                sign = len2; 
-			                ptr = malloc(sizeof(char) * (len1 + len2 + 1)); 
-			        } 
-	        else 
-		                ptr = malloc(sizeof(char) * (len1 + n + 1)); 
-	        if (ptr == NULL) 
-		                return (NULL); 
-	        for (count = 0; count < len1; count++) 
-		        { 
-			                ptr[count] = s1[count]; 
-			        } 
-	        for (count1 = 0; count1 < sign; count1++) 
-		        { 
-			                ptr[count++] = s2[count1]; 
-			        } 
-	        ptr[count++] = '\0'; 
-	        return (ptr); 
+	if (conct == NULL)
+		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
