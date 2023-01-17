2c2096538e3a2f7873ed3e0615665f8e8e68975c
LeoMcBills <leokinyera9@gmail.com>
2023-01-16 18:31:40 -0800

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,14 +17,14 @@ struct dog
 };
 
 /**
- * dogger - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog dogger;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dogger *new_dog(char *name, float age, char *owner);
-void free_dog(dogger *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
