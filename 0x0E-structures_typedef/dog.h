c8467f9f70c4a7f65fa7ee6c0c29cc167043ea92
LeoMcBills <leokinyera9@gmail.com>
2023-01-16 17:39:07 -0800

    Initialize variable to type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: firsf member of the structure
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dogger - typedef for struct dog
+ */
+
+typedef struct dog dogger;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dogger *new_dog(char *name, float age, char *owner);
+void free_dog(dogger *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
