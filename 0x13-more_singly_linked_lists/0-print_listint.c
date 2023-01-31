0fe7c0dfcc29e7638283aa63d4ce0aa7adb68ca0
LeoMcBills <leokinyera9@gmail.com>
2023-01-31 00:26:03 -0800

    numbers in a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer to head
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		number++;
+		h = h->next;
+	}
+	return (number);
+}
