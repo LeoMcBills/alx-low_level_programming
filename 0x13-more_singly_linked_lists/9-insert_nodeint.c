1ce7f7e4416c6ec77887224030716bbb5d6716b9
LeoMcBills <leokinyera9@gmail.com>
2023-01-31 21:22:09 -0800

    add node to a particular index of a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,4 +2,38 @@
 /**
  * insert_nodeint_at_index - insert new node at any given position
  * @head: pointer to head
- * @idx: 
+ * @idx: index of the list where the new node is to be added
+ * @n: the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+
+	listint_t *new;
+
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+	return (NULL);
+}
